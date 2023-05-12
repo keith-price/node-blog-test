b92137c576f65aa745c3d41236467ebd089c6244
Keith Price <keithp@pop-os.localdomain>
2023-05-12 09:28:58 +0700

    added searchbar and styling for admin

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const express_layout = require("express-ejs-layouts");
+const connectDB = require("./server/config/db");
 
 const app = express();
 
 const PORT = 5000 || process.env.PORT;
+
+// connect to mongo
+connectDB();
 
 app.use(express.static("public"));
 
